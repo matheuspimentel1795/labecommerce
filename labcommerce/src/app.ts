4fe325e364c2e5a449eee27f49c0a79dbb55aed2
matheuspimentel1795 <matheuspimentel1795@gmail.com>
2021-09-05 15:45:32 -0300

    dddd

--- labcommerce/src/app.ts
+++ labcommerce/src/app.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, ()=>{
+app.listen(process.env.PORT || 3003, ()=>{
     console.log('Servidor rodando na porta 3003')
 })
 
